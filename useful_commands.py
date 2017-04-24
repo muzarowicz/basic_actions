ec64756731060fcc72025dc50dee4daa117b5042
Mateusz Uzarowicz <mateusz.uzarowicz@getstreetteam.com>
2017-04-24 17:44:04 +0200

    next part

--- useful_commands.py
+++ useful_commands.py
@@ -1,2 +1,14 @@
 driver.find_element_by_xpath("(//*[@class='list-menu-element -t-option-download ng-scope'])[2]").click() # click on second item
 webdriver.find_element_by_xpath("(//*[@class='list-element-text more -t-more-options'])[position()=2]").click() # click on second item
+
+
+
+'''unicode
+
+
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+'''
