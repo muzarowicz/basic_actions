735e7df7202ac7a7c6e88af964228d3e2151ee85
mateusz uzarowicz <m.uzarowicz@clearcode.cc>
2017-04-25 15:47:32 +0200

    new part- phantomJS

--- get_attribute.py
+++ get_attribute.py
@@ -1,10 +1,22 @@
 from selenium import webdriver
+import pytest
 
-driver = webdriver.Chrome()
-driver.get("http://google.com")
-url = driver.current_url
-print (url)
-lst = driver.find_element_by_id('lst-ib')
-val = lst.get_attribute("title")
-print (val)
-driver.close()
+@pytest.fixture(scope='session')
+def webdriver():
+    #webdriver = webdriver.Chrome()
+    driver = webdriver.PhantomJS()
+    return driver
+
+def test_google(webdriver):
+    #driver = webdriver.PhantomJS("/usr/bin/phantomjs")
+    #driver = webdriver.Chrome()
+    driver.get("https://www.google.pl/")
+    url = driver.current_url
+    print (url)
+    assert url == 'https://www.google.pl/'
+def test_click(webdriver):
+    lst = driver.find_element_by_xpath("//input[@id='lst-ib']")
+    val = lst.get_attribute("title")
+    print (val)
+    print (url)
+    driver.close()
---
+++ phantomjs_second.py
@@ -0,0 +1,17 @@
+import pytest
+from selenium.webdriver import PhantomJS
+
+@pytest.fixture(scope='session')
+def webdriver(request):
+    driver = PhantomJS()
+    #driver = Chrome()
+    request.addfinalizer(driver.quit)
+    return driver
+
+def test_nix_website_title(webdriver):
+    webdriver.get('https://github.com')
+    assert 'GitHub' in webdriver.title
+
+def test_pytest_website_title(webdriver):
+    webdriver.get('https://twitter.com/?lang=en')
+    assert 'Twitter' in webdriver.title
