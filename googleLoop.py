e68c7159186af612aa09387583b989c8bcd6bcf2
mateusz uzarowicz <m.uzarowicz@clearcode.cc>
2017-05-09 15:39:05 +0200

    next part

--- googleLoop.py
+++ googleLoop.py
@@ -17,6 +17,7 @@ options.add_argument('--applicationCacheEnabled')
 options.add_argument('--acceptSslCerts')
 
 
+
 @pytest.fixture()
 def webdriver(request):
     driver = Chrome(executable_path='/usr/bin/chromedriver', chrome_options=options)
